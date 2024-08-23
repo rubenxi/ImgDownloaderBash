5d053755b48ff0686e93a78779d9931be55cc8bf
RubenXI <rubenmolinalozano.rml@gmail.com>
2024-08-23 13:02:35 +0000

    Update move.sh

--- move.sh
+++ move.sh
@@ -5,9 +5,9 @@ do
 
 if [[ -f "$entry" ]]
 then
-    sum=$(md5sum "$entry")
-  name=$(echo "${sum%% *}.${entry##*.}")
-   ln -fs "$(realpath "$entry")" ~/"ImgDownloaderBash/$name" > /dev/null 2>&1
+sum=$(md5sum "$entry")
+name=$(echo "${sum%% *}.${entry##*.}")
+ln -fs "$(realpath "$entry")" ~/"ImgDownloaderBash/$name" > /dev/null 2>&1
 fi
 done
 
